a91d8314faf43c89b87f7f8963dcde477ecc9b0f
이성용 <sy.lee@dna.uno>
2017-03-31 15:47:19 +0900

    뉴스 건수 추출 알고리즘 수정 2

--- crawler/views.py
+++ crawler/views.py
@@ -86,23 +86,26 @@ def getNumberOfNews(query):
 		headers = { 'User-Agent' : user_agent }
 		r = urllib2.Request(query, headers=headers)
 		URL_source_FOR_DATE = urllib2.urlopen(r)
+
+	except Exception as e:
+		logging.error(e)
+		pass
+
+	if(URL_source_FOR_DATE): 
 		logging.info("url query success !!")
 		soup = BeautifulSoup(URL_source_FOR_DATE, 'lxml', from_encoding='utf-8')
 
-		# # 기사 없음 
-		# if not soup.find('div', 'title_desc all_my'):
-		# 	URL_source_FOR_DATE.close()
-		# 	return 0
+		# 기사 없음 
+		if not soup.find('div', 'title_desc all_my'):
+			URL_source_FOR_DATE.close()
+			return 0
 
 		news_num_for_day = soup.find('div', 'title_desc all_my').select('span')[0].text.split('/')
 		news_num_for_day_int = int(filter(lambda x: x.isdigit(), news_num_for_day[1])) # 숫자 추출 
 		URL_source_FOR_DATE.close()
 		return  news_num_for_day_int
-
-	except Exception as e:
-		logging.error(e)
+	else:
 		return 0
-		
 
 def get_content(url):
 	print "searching..."
