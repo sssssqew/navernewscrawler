1fa2ffd0d1637f3295c6bbb7cfa796f9f6fb2868
이성용 <sy.lee@dna.uno>
2017-04-05 15:31:13 +0900

    DONUTCRAWL-27

--- crawler/admin.py
+++ crawler/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Keyword
+
 # Register your models here.
-admin.site.register(Keyword)
+class KeywordAdmin(admin.ModelAdmin):
+	search_fields = ['name','category', 'donut', 'created_date']
+	
+admin.site.register(Keyword, KeywordAdmin)
